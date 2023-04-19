013b0f008a7ffa4a132097b33faa18e3165c3b82
Dinesh J <datadinesh1@gmail.com>
2023-04-19 11:59:52 +0530

    sec commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## End to End Machne Learning Project
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+
+e_dot="-e."
+
+def get_requirements(file_path:str)->List[str]:
+    '''Ths function will return a list of requirements'''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n',"") for req in requirements]
+
+        if e_dot in requirements:
+            requirements.remove(e_dot)
+    return requirements
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Dinesh",
+    author_email="datadinesh1@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    )
